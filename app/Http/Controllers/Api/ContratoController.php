531a728abb8c66a3121f74f418b0c7ac4b56232c
poma <poma6185@gmail.com>
2023-12-19 16:46:50 -0400

    v4

--- app/Http/Controllers/Api/ContratoController.php
+++ app/Http/Controllers/Api/ContratoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\DetalleCByContratoRequest;
+use App\Models\ClienteHasContrato;
 use Illuminate\Http\Request;
 //add
 use App\Models\Desarrolladora;
@@ -64,24 +65,38 @@ class ContratoController extends Controller
     public function store(DetalleCByContratoRequest $request)
     {
         try {
+            $desarrolladora = Desarrolladora::where('status', true)
+                ->where('nombres', $request->input('desarrolladora'))
+                ->first();
+            //debemos verificar si la desarrolladora existe en la base de datos por seguridad
+            if ($desarrolladora == null) {
+                return response()->json([
+                    'records' => null,
+                    'status' => false,
+                    'message' => "No se encontro la desarrolladora {$request->input('desarrolladora')}",
+                ], 404);
+            }
+
             //el $fillable del modelo son todos los campos que se insertan en la base de datos
             //si el campo a insertar no esta listado en el array $fillable entonces
             //ese campo no se insertara en la base de datos, aunque en $request->all() este ese campo
             //ese campo sera omitido
             $contrato = new Contrato($request->all());
-            $contrato->n_contrato = $this->generateNumContrato($request->input('ciudad'));
+            $contrato->n_contrato = $this->generateNumContrato();
             $contrato->status = true;
             $contrato->save();
 
-            $detalle_contrato = new DetalleContrato($request->except('ciudad'));
+            $detalle_contrato = new DetalleContrato($request->all());
             $detalle_contrato->status = true;
             $detalle_contrato->id_contrato = $contrato->id;
             $detalle_contrato->save();
 
-           
+            $cliente_has_contrato = new ClienteHasContrato();
+            $cliente_has_contrato->id_contrato = $contrato->id;
+            $cliente_has_contrato->id_cliente = $request->input('id_cliente');
 
             $contrato->archivo_pdf = $this->generatePdfContrato($contrato->id);
-            $contrato->save();
+            $contrato->update();
 
             return response()->json([
                 'status' => true,
@@ -97,47 +112,35 @@ class ContratoController extends Controller
         }
     }
 
-    public function generateNumContrato(string $ciudad)
+    public function generateNumContrato()
     {
-        $ciudades_key = [
-            'Santa-Cruz' => 'SC',
-            'Chuquisaca' => 'CH',
-            'Cochabamba' => 'CB',
-            'Potosi' => 'PT',
-            'Beni' => 'BN',
-            'La-Paz' => 'LP',
-            'Pando'  => 'PA',
-            'Tarija'  => 'TJ',
-            'Oruro' => 'OR',
-            'Otros' => 'OTS',
-        ];
-
         //obtenemos ultimo contrato registrato por ciudad
         //y no necesitamos verificar el status
         // ya que aunque el registro se haya eliminado igualmente contatos los registros de contratos para generar un n_contrato
-        $ultimo_contrato_id = Contrato::join('clientes', 'clientes.id', '=', 'contratos.id_cliente')
-            ->join('grupos', 'grupos.id', '=', 'clientes.id_grupo')
-            ->join('ciudades', 'ciudades.id', '=', 'grupos.id_ciudad')
-            ->select('clientes.nombres', 'clientes.apellido_paterno', 'clientes.apellido_materno', 'contratos.*')
-            ->where('ciudades.city_name',  $ciudad)
+        $ultimo_contrato_id = Contrato::join('detalle_contratos', 'detalle_contratos.id_contrato', '=', 'contratos.id')
+            ->join('clientes_has_contratos', 'clientes_has_contratos.id_contrato', '=', 'contratos.id')
+            ->join('clientes', 'clientes.id', '=', 'clientes_has_contratos.id_cliente')
+            ->select(
+                'contratos.*',
+            )
+            ->where('clientes.status', true)
+            ->where('contratos.status', true)
+            ->where('detalle_contratos.status', true)
             ->max('contratos.id');
+
         $ultimo_contrato = Contrato::find($ultimo_contrato_id);
 
-
-        // verificamos si es el primer registro, entonces es nulo y asignamos el numero cero
-        $old_numero_contrato = ($ultimo_contrato == null) ? '0000/0000_AS' : $ultimo_contrato->n_contrato;
+        // verificamos si es el primer registro, entonces es nulo y asignamos el numero 0000/2023
+        $old_numero_contrato = ($ultimo_contrato == null) ? '0000/2023' : $ultimo_contrato->n_contrato;
 
         $parts = explode('/', $old_numero_contrato); // Divide la cadena en partes usando "/"
-        $middle_part = $parts[1]; // Obtiene la segunda parte, que es '009_LP'
-        $sub_parts = explode('_', $middle_part); // Divide la parte intermedia usando "_"
-        $num_result = $sub_parts[0]; // Obtiene la primera subparte, que es '009'
+        $num_result = $parts[0]; // Obtiene la primera subparte, que es '0000'
 
         $num_result = $num_result + 1;
         $num_result = str_pad($num_result, 4, '0', STR_PAD_LEFT); //agregar ceros al numero
         $year =  date('Y');;
-        return  "{$year}/{$num_result}_{$ciudades_key[$ciudad]}";
+        return  "{$num_result}/{$year}";
     }
-
 
     public function update(DetalleCByContratoRequest $request,)
     {
@@ -159,7 +162,8 @@ class ContratoController extends Controller
             $contrato->update();
 
             // si todo es ok entonces eliminados el archivo pdf antiguo
-            Storage::disk('public')->delete(str_replace("storage/", "", $antiguo_pdf_path));
+            $parse_path_pdf = str_replace('/storage', "", $antiguo_pdf_path);
+            Storage::disk('public')->delete($parse_path_pdf);
 
             return response()->json([
                 'status' => true,
@@ -199,10 +203,12 @@ class ContratoController extends Controller
         }
     }
 
-    public function rowTableroByIdDetalleContrato(Request $request)
+    public function showDetalleContrato(Request $request)
     {
         try {
-            $detalle_contrato = DetalleContrato::where('status', true)->where('id_contrato', $request->input('id_contrato'))->first();
+            $detalle_contrato = DetalleContrato::where('status', true)
+                ->where('id_contrato', $request->input('id_contrato'))
+                ->first();
 
             return response()->json([
                 'status' => true,
@@ -213,15 +219,16 @@ class ContratoController extends Controller
             return response()->json([
                 'status' => false,
                 'message' => $th->getMessage(),
-                'record' => [],
+                'record' => null,
             ], 500);
         }
     }
 
     public function generatePdfContrato(int $id)
     {
-        $contrato = Contrato::join('clientes', 'clientes.id', '=', 'contratos.id_cliente')
-            ->join('detalles_contratos', 'detalles_contratos.id_contrato', '=', 'contratos.id')
+        $contrato_cliente = Contrato::join('detalle_contratos', 'detalle_contratos.id_contrato', '=', 'contratos.id')
+            ->join('clientes_has_contratos', 'clientes_has_contratos.id_contrato', '=', 'contratos.id')
+            ->join('clientes', 'clientes.id', '=', 'clientes_has_contratos.id_cliente')
             ->select(
                 'clientes.nombres',
                 'clientes.apellido_paterno',
@@ -229,21 +236,22 @@ class ContratoController extends Controller
                 'clientes.ci',
                 'clientes.ci_expedido',
                 'contratos.*',
-                'detalles_contratos.*',
+                'detalle_contratos.*'
             )
             ->where('clientes.status', true)
             ->where('contratos.status', true)
-            ->where('detalles_contratos.status', true)
+            ->where('detalle_contratos.status', true)
             ->where('contratos.id', $id)
             ->first();
 
-        $pdf = PDF::loadView('contrato/pdf-contrato-ciudad', ['contrato' => $contrato,]);
-        //el tamaño del papel no esta definido
+        $pdf = PDF::loadView('pdf/contrato/contrato-pdf', ['contrato_cliente' => $contrato_cliente,]);
+        //$pdf->setPaper((array(0, 0, 612.00, 1008.00)),'landscape');//oficio horizontal
+        $pdf->setPaper('legal', 'portrait'); //ocicio  vertical
 
         //debemos parsear el numero de contrato porque tiene el formato '2023/0001_LP' 
         // no se puede guardar archivos con ese tipo de nombre entonces  obtenemos el nombre de este formato '2023_0001_LP'
-        $n_contrato_parse = str_replace('/', '_', $contrato->n_contrato);
-        $pdf_path = "pdfs-contratos/{$n_contrato_parse}_{$contrato->nombres}_{$contrato->apellido_paterno}_{$contrato->apellido_materno}_" . uniqid() . ".pdf";
+        $n_contrato_parse = str_replace('/', '_', $contrato_cliente->n_contrato);
+        $pdf_path = "pdf/contrato/{$n_contrato_parse}-{$contrato_cliente->nombres}-{$contrato_cliente->apellido_paterno}-{$contrato_cliente->apellido_materno}-" . uniqid() . ".pdf";
 
         Storage::disk('public')->put($pdf_path,  $pdf->download()->getOriginalContent());
         return  "/storage/{$pdf_path}";
