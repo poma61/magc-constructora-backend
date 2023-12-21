81eb84925a7358e5812ca16edc556b4ff9111fac
poma <poma6185@gmail.com>
2023-12-21 14:27:32 -0400

    v8

--- app/Http/Requests/DetalleCByContratoRequest.php
+++ app/Http/Requests/DetalleCByContratoRequest.php
@@ -54,6 +54,12 @@ class DetalleCByContratoRequest extends FormRequest
             'detalle_contrato.lugar_firma_contrato' => 'required',
             'detalle_contrato.fecha_firma_contrato' => 'required|date',
         ];
+        if ($this->input('detalle_contrato.add_info_terreno')) {
+            $rules['detalle_contrato.terreno_valor_total_numeral'] = 'required|numeric';
+            $rules['detalle_contrato.terreno_valor_total_literal'] = 'required|string';
+            $rules['detalle_contrato.terreno_val_couta_inicial_numeral'] = 'required|numeric';
+            $rules['detalle_contrato.terreno_val_couta_mensual_numeral'] = 'required|numeric';
+        }
 
         // Agregar reglas para cada cliente en el array
         foreach ($this->input('clients', []) as $key => $client) {
@@ -89,11 +95,73 @@ class DetalleCByContratoRequest extends FormRequest
                 ];
             }
         }
-
-
-
         return $rules;
     }
+
+    public function messages(): array
+    {
+        $messages = [
+            'id_personal.required' => 'El campo personal es requerido.',
+            'type_role.required' => 'El campo rol es requerido.',
+            'password.required' => 'El campo contraseña es requerido.',
+            'password.min' => 'El campo contraseña debe tener al menos 8 caracteres.',
+
+            //detalle contrato
+            'detalle_contrato.n_de_lote.required' => 'El campo personal es requerido.',
+            'detalle_contrato.n_de_uv.required' => 'El campo personal es requerido.',
+            'detalle_contrato.zona.required' => 'El campo personal es requerido.',
+            'detalle_contrato.terreno_superficie.required' => 'El campo personal es requerido.',
+            'detalle_contrato.numero_distrito.required' => 'El campo personal es requerido.',
+            'detalle_contrato.numero_identificacion_terreno.required' => 'El campo personal es requerido.', //puede string string o numero
+            'detalle_contrato.norte_medida_terreno.required' => 'El campo personal es requerido.',
+            'detalle_contrato.norte_colinda_lote.required' => 'El campo personal es requerido.',
+            'detalle_contrato.sur_medida_terreno.required' => 'El campo personal es requerido.',
+            'detalle_contrato.sur_colinda_lote.required' => 'El campo personal es requerido.',
+            'detalle_contrato.este_medida_terreno.required' => 'El campo personal es requerido.',
+            'detalle_contrato.este_colinda_lote.required' => 'El campo personal es requerido.',
+            'detalle_contrato.oeste_medida_terreno.required' => 'El campo personal es requerido.',
+            'detalle_contrato.oeste_colinda_lote.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_descripcion.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_superficie_terreno.required' => '',
+            'detalle_contrato.construccion_valor_total_literal.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_valor_total_numeral.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_cantidad_meses_de_entrega.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_val_couta_inicial_literal.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_val_couta_inicial_numeral.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_val_couta_mensual_literal.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_val_couta_mensual_numeral.required' => 'El campo personal es requerido.',
+            'detalle_contrato.construccion_cantidad_couta_mensual.required' => 'El campo personal es requerido.',
+            'detalle_contrato.primera_val_couta_mensual_numeral.required' => 'El campo personal es requerido.',
+            'detalle_contrato.segunda_val_couta_mensual_numeral.required' => 'El campo personal es requerido.',
+            'detalle_contrato.tercera_val_couta_mensual_numeral.required' => 'El campo personal es requerido.',
+            'detalle_contrato.lugar_firma_contrato.required' => 'El campo personal es requerido.',
+            'detalle_contrato.fecha_firma_contrato.required' => 'El campo personal es requerido.',
+            'detalle_contrato.fecha_firma_contrato.date' => 'El campo fecha firma contrato no es una fecha válida',
+            //add info terreno
+            'detalle_contrato.terreno_valor_total_numeral' => 'El campo personal es requerido.',
+            'detalle_contrato.terreno_valor_total_literal' => 'El campo personal es requerido.',
+            'detalle_contrato.terreno_val_couta_inicial_numeral' => 'El campo personal es requerido.',
+            'detalle_contrato.terreno_val_couta_mensual_numeral' => 'El campo personal es requerido.',
+
+        ];
+
+        foreach ($this->input('clients', []) as $key => $client) {
+            //clients=> es el nombre del array de objetos que se envia desde el frontend
+            $messages["clients.{$key}.id"] = 'El campo personal es requerido.'; 
+            $messages["clients.{$key}.nombres"] = 'El campo personal es requerido.';
+            $messages["clients.{$key}.apellido_paterno"] = 'El campo personal es requerido.';
+            $messages["clients.{$key}.apellido_materno"] = 'El campo personal es requerido.';
+            $messages["clients.{$key}.n_de_contacto"] = 'El campo personal es requerido.';
+            $messages["clients.{$key}.ci.required"] = 'El campo personal es requerido.';
+            $messages["clients.{$key}.ci_expedido.required"] = 'El campo personal es requerido.';
+            $messages["clients.{$key}.direccion.required"] = 'El campo direccion es requerido.';
+            $messages["clients.{$key}.correo_electronico.email"] = 'El formato del correo electronico no es válido.';
+            $messages["clients.{$key}.correo_electronico.unique"] = 'El campo correo electronico ya ha sido tomado.';
+        }
+
+        return $messages;
+    }
+
 
     protected function failedValidation(Validator $validator): HttpResponseException
     {
